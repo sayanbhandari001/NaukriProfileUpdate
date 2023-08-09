42cf39de27ed0d38533c55a7c6f6f625e4a69228
Sayan <Sayan@sayanlp>
2023-08-09 16:44:46 +0530

    updates till today

--- NaukriProfileUpdateViaBDD/src/test/java/com/utility/BrowserUtility.java
+++ NaukriProfileUpdateViaBDD/src/test/java/com/utility/BrowserUtility.java
@@ -13,20 +13,20 @@ public class BrowserUtility {
 
 	public static WebDriver supplyDriver(String browser) {
 		if (browser.equals("chrome")) {
-			ChromeOptions options = new ChromeOptions();
-			options.addArguments("--headless");
-			options.addArguments("window-size=1400,1500");
-			options.addArguments("--disable-gpu");
-			options.addArguments("--no-sandbox");
-			options.addArguments("start-maximized");
-			options.addArguments("enable-automation");
-			options.addArguments("--disable-infobars");
-			options.addArguments("--disable-dev-shm-usage");
-			// ChromeOptions co = new ChromeOptions();
-			// co.setBrowserVersion("116");
+//			ChromeOptions options = new ChromeOptions();
+//			options.addArguments("--headless");
+//			options.addArguments("window-size=1400,1500");
+//			options.addArguments("--disable-gpu");
+//			options.addArguments("--no-sandbox");
+//			options.addArguments("start-maximized");
+//			options.addArguments("enable-automation");
+//			options.addArguments("--disable-infobars");
+//			options.addArguments("--disable-dev-shm-usage");
+			// Set version 116
+			ChromeOptions co = new ChromeOptions();
+			co.setBrowserVersion("116");
 			return new ChromeDriver();
 		} else if (browser.equals("firefox")) {
-
 			return new FirefoxDriver();
 		} else if (browser.equals("ie")) {
 			WebDriverManager.iedriver().setup();
